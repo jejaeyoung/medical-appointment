6667d9caf0fe50ed746a5f69bce348cc2ae1e7aa
Daniel Sebastian G. Quilatan <98928754+jejaeyoung@users.noreply.github.com>
2023-10-29 08:04:16 +0000

    Last Change

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ function App() {
       <p>Daniel Sebastian G. Quilatan</p>
       <p>Project Manager</p>
       <p>jejaeyoung</p>
-      <p>asdadasd</p>
-      asdkashjdklllxvm
-      asdfafafgfgfgf ang cute
+     
       <hr/>
 
     </>
