d2afd38175f067745f32d9991ee5a3ca700fb10e
Daniel Sebastian G. Quilatan <quilatandg@students.nu-moa.edu.ph>
2023-11-16 00:14:54 +0800

    gahahhahahah

--- backend/controllers/user_controller.js
+++ backend/controllers/user_controller.js
@@ -1,8 +1,9 @@
-const User = require('../models/user_collection');
+const Users = require('../models/user_model');
+
 
 
 const NewUserSignUp = (req, res) => {
-    User.create(req.body)
+    Users.User.create(req.body)
     .then((newUser) => {
         res.json({newUser: newUser, status:"Successfully registered user."})
     })
@@ -11,6 +12,29 @@ const NewUserSignUp = (req, res) => {
     });
 }
 
+const userEmail = 'user@example.com';
+
+const addNewPost = (req, res) => {
+    const userEmail = req.body.email; // Assuming you pass user email in the request body
+    const newPostContent = req.body.post; // Assuming you pass new post content in the request body
+
+    Users.User.findOne({ email: userEmail })
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({ message: 'User not found' });
+            }
+            user.post.push(newPostContent);
+            return user.save();
+        })
+        .then(updatedUser => {
+            res.json({ updatedUser, message: 'New post added successfully' });
+        })
+        .catch(error => {
+            res.json({ message: 'Error adding post', error });
+        });
+}
+
 module.exports = {
     NewUserSignUp,
+    addNewPost
 }
--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -2,6 +2,8 @@ const UserController = require ('../controllers/user_controller');
 // const { validationResult } = require('express-validator');
 
 module.exports = app => { 
+    app.get('/api/test',(req,res)=>{res.json({message:"the api is working"})});
     app.post('/api/medapp/signup', UserController.NewUserSignUp);
+    app.post('/api/medapp/addpost', UserController.addNewPost);
 
 }
