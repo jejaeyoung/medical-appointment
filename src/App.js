92550ab0584ea674125a08af09f7e14d843307f5
Daniel Sebastian G. Quilatan <quilatandg@students.nu-moa.edu.ph>
2023-11-17 22:26:18 +0800

    Edited SignUp

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 //Components
 import LandingPage from './components/landpage/LandingPage';
 import Login from './components/login/Login';
+import NewSignUp from './components/login/NewSignUp';
 
 //Practitioner
 import Dashboard from './components/practitioner/dashboard/Dashboard';
@@ -18,11 +19,10 @@ import EditMode from './components/practitioner/editmode/EditMode';
 function App() {
   return (
     <>
-    <p>dsadadd</p>
     <BrowserRouter>
       <Routes>
         <Route path={'/'} element={<LandingPage/>}></Route>
-        <Route path='medapp/signup' element={<NewSignUp/>}> </Route>
+        <Route path={'/medapp/signup'} element={<NewSignUp/>}> </Route>
         <Route path={"/practitioner/dashboard"}element={<Dashboard/>}/>
         <Route path={"/practitioner/dashboard/edit/:uid/:index"}element={<EditMode/>}/>  
       </Routes>
